b0f3926b3218b37050578f9e9eca4ae243fd40d2
Matteo Bresciani <matteo.bresciani.997@gmail.com>
2020-02-01 00:08:54 +0100

    added world generation script

---
+++ Amelie/Assets/Scripts/terrainManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrainManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> terrain;
+    private List<int> nullIndices;
+
+    private int lastInsertedPosition;
+
+    //sprite variables
+    [SerializeField]
+    private float spriteLength;
+
+    [SerializeField]
+    private float startX;
+
+    [SerializeField]
+    private float fixedY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastInsertedPosition = -1;
+        nullIndices = new List<int>();
+        initializeSpaceAndNullIndices();
+    }
+
+    private void initializeSpaceAndNullIndices()
+    {
+        float currentX = startX;
+        for (int i = 0; i < terrain.Count; i++, currentX += spriteLength)
+        {
+            if (terrain[i] != null)
+            {
+                Instantiate(terrain[i], new Vector3(currentX, fixedY, 0), Quaternion.identity);
+            }
+            else
+            {
+                nullIndices.Add(i);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+            insert(terrain[0]);
+        if (Input.GetKeyDown(KeyCode.G))
+            destroyLast();
+    }
+
+    public void insert(GameObject obj)
+    {
+        for(int i = 0; i < nullIndices.Count; i++)
+        {
+            if (terrain[nullIndices[i]] == null)
+            {
+                GameObject newInstance = Instantiate(obj, new Vector3(startX + nullIndices[i] * spriteLength, fixedY, 0), Quaternion.identity);
+                terrain[nullIndices[i]] = newInstance;
+                lastInsertedPosition = nullIndices[i];
+                return;
+            }
+        }
+    }
+
+    public void destroyLast()
+    {
+        for (int i = nullIndices.Count - 1; i >= 0; i--)
+        {
+            if (terrain[nullIndices[i]] != null)
+            {
+                Destroy(terrain[nullIndices[i]]);
+                terrain[nullIndices[i]] = null;
+                return;
+            }
+        }
+    }
+}
