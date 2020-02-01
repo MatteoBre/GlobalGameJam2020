1a2329269957ec3df75b7ea8e22854eeeea400a0
Matteo Bresciani <matteo.bresciani.997@gmail.com>
2020-02-01 14:04:42 +0100

    StartingLevel updated with functioning gameplay and camera following player

---
+++ Amelie/Assets/follow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objToFollow;
+
+    private float minX;
+    [SerializeField]
+    private float maxX;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minX = gameObject.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Max(objToFollow.transform.position.x, minX) + offset.x, offset.y, offset.z); // Camera follows the player with specified offset position
+    }
+}
