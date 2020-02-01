a7817d6b563fa66be3812c9e76818722b0c10daa
Matteo Bresciani <matteo.bresciani.997@gmail.com>
2020-02-01 15:36:56 +0100

    added fading

---
+++ Amelie/Assets/Scripts/fading.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fading : MonoBehaviour
+{
+    [SerializeField]
+    private float seconds;
+    private float elapsed;
+
+    private bool countDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDown = false;
+        elapsed = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countDown)
+        {
+            elapsed += Time.deltaTime;
+            if (elapsed >= seconds)
+                Destroy(gameObject);
+            updateSprites();
+        }
+    }
+
+    private void updateSprites()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.GetComponent<SpriteRenderer>().color = new Vector4(1, 1, 1, 1f - elapsed/seconds);
+        }
+    }
+
+    public void activateCountDown()
+    {
+        countDown = true;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        activateCountDown();
+    }
+}
