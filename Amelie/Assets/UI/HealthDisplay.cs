c2bb65d2a2b744368be0a6408ee43e82f8101786
Lucio <satorlu@yahoo.com.br>
2020-02-01 13:11:08 +0100

    first push

---
+++ Amelie/Assets/UI/HealthDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private int health = 5;
+    public Text healthText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Amelie's Health: " + health;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health++;
+        }
+
+    }
+}
