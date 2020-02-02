c27daa8801cefac6f0f2e5e9a1fc7f1f76ccb927
Matteo Bresciani <matteo.bresciani.997@gmail.com>
2020-02-02 12:09:04 +0100

    rabbit added

---
+++ Amelie/Assets/Scripts/levelModifier.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelModifier : MonoBehaviour
+{
+    [SerializeField]
+    private int modifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getLevelModifier()
+    {
+        return modifier;
+    }
+}
