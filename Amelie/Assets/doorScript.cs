936c3c406b75febc8cb976f057f0ce7f2733ba2f
Matteo Bresciani <matteo.bresciani.997@gmail.com>
2020-02-02 10:41:50 +0100

    door of end level added

---
+++ Amelie/Assets/doorScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<Animator>().Play("doorAnim");
+        StartCoroutine(ActivateParticles());
+    }
+
+    private IEnumerator ActivateParticles()
+    {
+        yield return new WaitForSeconds(1);
+        particles.SetActive(true);
+        yield return new WaitForSeconds(2);
+        GameObject.FindGameObjectWithTag("fader").GetComponent<fadeOut>().startFading();
+    }
+}
---
+++ Amelie/Assets/fadeOut.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fadeOut : MonoBehaviour
+{
+    private bool fade;
+    private float fadeSeconds = 3;
+    private float timeElapsed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fade)
+            timeElapsed += Time.deltaTime;
+        GetComponent<Image>().color = new Color(1, 1, 1, 0f + timeElapsed / fadeSeconds);
+
+        if(timeElapsed >= fadeSeconds)
+        {
+            //load scene
+        }
+    }
+
+    public void startFading()
+    {
+        fade = true;
+    }
+}
