2dc24fbe7740fd41aba07c8b520df5dc3bb13879
Matteo Bresciani <matteo.bresciani.997@gmail.com>
2020-02-02 13:11:54 +0100

    end scene and rabbit edited

---
+++ Amelie/Assets/cameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    public float startingSize;
+    public float endSize;
+
+    public float seconds;
+    private float timeElapsed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeElapsed <= seconds)
+            timeElapsed += Time.deltaTime;
+        GetComponent<Camera>().orthographicSize = startingSize + (endSize - startingSize) * (timeElapsed / seconds);
+    }
+}
